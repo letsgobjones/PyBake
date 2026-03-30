2c7b84268cef9f84ca69cd32fffc068d0a5930f5
Brandon <89342970+letsgobjones@users.noreply.github.com>
2026-03-30 20:20:59 +0800

    feat(textnode): implement conversion from TextNode to HTMLNode
    Added the text_node_to_html_node function to handle the transformation of various text types into their corresponding HTML leaf nodes. This enables the integration between the internal markdown representation and the HTML rendering engine.
    
    BREAKING CHANGE: TextType.PLAIN has been renamed to TextType.TEXT to better reflect its semantic purpose.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,8 @@
+from typing import ValuesView
 import unittest
 
-from textnode import TextNode, TextType
+import htmlnode
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -35,9 +37,42 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node, node2)
 
     def test_empty_test(self):
-        node = TextNode("", TextType.PLAIN)
-        node2 = TextNode("", TextType.PLAIN)
+        node = TextNode("", TextType.TEXT)
+        node2 = TextNode("", TextType.TEXT)
         self.assertEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_node_to_html_bold(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+
+    def test_text_node_to_html_node_link(self):
+        node = TextNode("Click Me", TextType.LINK, "https://www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click Me")
+        self.assertEqual(html_node.props, {"href": "https://www.google.com"})
+
+    def test_text_node_to_html_node_image(self):
+        node = TextNode("Description of image", TextType.IMAGE, "https://image.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props, {"src": "https://image.png", "alt": "Description of image"}
+        )
+
+    def test_text_node_to_html_node_exception(self):
+        node = TextNode("Oops", "NOT_A_TYPE")
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
 
 
 if __name__ == "__main__":
